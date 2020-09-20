d50928285c94f22373d30bd2af16d20b1ed28013
pacheco <pacheco@gmx.de>
2020-09-20 11:56:59 +0200

    Changed landing page text

--- capstone/src/main/java/de/pacheco/capstone/ui/home/HomeViewModel.java
+++ capstone/src/main/java/de/pacheco/capstone/ui/home/HomeViewModel.java
@@ -10,7 +10,12 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Hello my name is Pacheco and this App is a collection of some smaller " +
+                "Apps i have created while getting my app creation skills up to date. I love to " +
+                "make great Apps with" +
+                " a nice User Experience. As of september " +
+                "2020 this app is made with state of the art technologies. But you know you never" +
+                " stop learning. Checkout the source code if you like: https://github.com/pachecoberlin/Capstone-Project");
     }
 
     public LiveData<String> getText() {
