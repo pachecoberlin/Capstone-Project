9acd33414c901fe7ce9fc907849173a562108f3c
pacheco <pacheco@gmx.de>
2021-05-15 19:56:18 +0200

    Started conversion to Kotlin. Mainactivity is converted. Donation to paypal now possible.

---
+++ capstone/src/main/java/de/pacheco/capstone/MainActivity.kt
@@ -0,0 +1,61 @@
+package de.pacheco.capstone
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    private var mAppBarConfiguration: AppBarConfiguration? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_capstone)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener(heartButton())
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        mAppBarConfiguration = AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_movies, R.id.nav_jokes,
+                R.id.nav_baking, R.id.nav_sandwich)
+                .setOpenableLayout(drawer)
+                .build()
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration!!)
+        NavigationUI.setupWithNavController(navigationView, navController)
+    }
+
+    private fun heartButton() = { view: View? ->
+        val snack = Snackbar.make(view!!, "Thank's I love it  \uD83D\uDE09 ", Snackbar
+                .LENGTH_LONG)
+        snack.setAction(R.string.donate_with_paypal) {
+            val url = "https://www.paypal.me/pachecoberlin"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+        snack.show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        getMenuInflater().inflate(R.menu.main, menu);
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        return (NavigationUI.navigateUp(navController, mAppBarConfiguration!!)
+                || super.onSupportNavigateUp())
+    }
+}
